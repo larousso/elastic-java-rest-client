759f95819ac14afdf69740473616311b5c962608
adelegue <adelegue@hotmail.com>
2018-01-24 11:30:38 +0100

    CI

--- build.sh
+++ build.sh
@@ -5,11 +5,13 @@ if test "$TRAVIS_PULL_REQUEST" = "false"
 then
     if [ -z "$TRAVIS_TAG" ];
     then
-      ./gradlew test
+        echo "Running tests ..."
+        ./gradlew test
     else
         echo "Deploying to bintray"
         ./gradlew bintrayUpload
     fi
 else
+    echo "Running tests ..."
     ./gradlew test
 fi
